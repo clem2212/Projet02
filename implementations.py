85fbf14004c7b2a738679cb9deab48c6a5bf23a1
VD2401 <102732099+VD2401@users.noreply.github.com>
2022-12-05 15:44:20 +0100

    ML on avance wesh

--- implementations.py
+++ implementations.py
@@ -80,246 +80,97 @@ def clean_df(df):
           "X(mm)_s","Y(mm)_s","Z(mm)_s",'DX_s', 'DY_s', 'DZ_s','Kin(MeV)_s']]\
     .apply(pd.to_numeric)
     
-    
     return df_new
 
 
+def type_to_num(ptcl):
+    #Each particle is associated to an integer number 
+    if(ptcl == 'gamma'):
+        return 2
+    elif(ptcl == "e-"):
+        return 1
+    elif(ptcl == 0):
+        return 0
+    else:
+        raise Exception('Not a good type of particle')
 
-def input_output_bis(df):
-    """ This function create two dataframe X of input and the Y of output, it would be neccessary to train 
-    our data and creating a model for future prediciton 
-    
-    Return : 
-        X : The input data composed of the following feature size (nb_inputs, 7)
-            - pos (x,y,z)
-            - dir (dx,dy,dz)
-            - E 
-            
-        Y : The output data, of size (nb_inputs, 7)
-            - dE
-            - distance=steplength
-            - cos(psi) nouvelle trajectoir
-            - Q (pos_s, dir_s, type_s, E_s)
-        
-    """
-    pd.options.mode.chained_assignment = None
-    df_input = df_clean[["X(mm)","Y(mm)","Z(mm)","DX","DY","DZ","KinE(MeV)"]]
-    
-    df_output = df_clean[['dE(MeV)', 'StepLeng', "X(mm)_s","Y(mm)_s","Z(mm)_s",\
-                'DX_s', 'DY_s', 'DZ_s','Kin(MeV)_s', 'name_s']]
-
-    df_output['dE(MeV)'] = df_output['dE(MeV)'] + df_output['Kin(MeV)_s']
-    
-    df_output
-    
-
-def input_output(df) :
-    """ This function create two dataframe X of input and the Y of output, it would be neccessary to train 
-    our data and creating a model for future prediciton 
-    
-    Return : 
-        X : The input data composed of the following feature size (nb_inputs, 5)
-            - cos_teta
-            - Kin_E  <-- Arreter le programme quand Kin_E = 0 ! --> gérer les simulations dans ce sens
-            - dE  <-- Se calcule juste comme la différence entre les 2 énergies qu'on a !
-            - Step Lenght
-            - Proc Name
-            
-        Y : The output data, of size (nb_inputs, 4)
-            - distance
-            - delta_e
-            - cos_teta
-            - Q
-        
-    """
-    #conversion of the dataframe into a numpy array :
-    np_data = df_clean.to_numpy()
-    X = []
-    Y = [0, 0, 0, 0]
-    
-    for idx, data_x in enumerate(data) :
-    # Until we are at the end of the simulation (We don't iterate on the last element as we access in this loof the i+1 element) 
-        if (idx != data.shape[0]-1) :
-            if (data[idx+1] != 0) :
-                line_input = []
-                line_output = []
-                #Creation of the line idx of the matrix X
-                
-                #1st element:cos_teta = (dx).(dx') scalar product of the postions vectors (dx, dy, dz) at the postions i and i+1
-                # data_x[1] = Dx, data_x[2] = Dy, data_x[3] = Dz
-                cos_teta_before = data[idx-1, 1]*data_x[1] + data[idx-1, 2]*data_x[2] + data[idx-1, 3]*data_x[3]
-                cos_teta_after = data[idx+1, 1]*data_x[1] + data[idx+1, 2]*data_x[2] + data[idx+1, 3]*data_x[3]
-                line_input.append(cos_teta_before)
-                line_output.append(cos_teta_after)
-                
-                #2,3th elements : energy steps
-                KinE_before = data_x[4]
-                KinE_after = data[idx + 1, 4]
-                line_input.append(KinE_before)
-                line_output.append(KinE_after)
-                
-                dE_before = data_x[5]
-                dE_after = data[idx + 1, 5]
-                line_input.append(dE_before)
-                line_output.append(dE_after)
-                
-                #4th element : step Length
-                step_before = data_x[6]
-                step_after = data[idx + 1, 6]
-                line_input.append(step_before)
-                line_output.append(step_after)
-                
-                #5th element : type of interactions (We have to use this parameter to create predict the type of interaction!
-                # no change in the type of interaction : 0
-                # change from msc to eIoni : 1 and the inverse -1
-                # change from msc to eBrem : 2 and the inverse -2
-                # change from eIoni to eBrem : 3 and the inverse -3
-                
-                name_nb = 0
-                name_before = data_x[7]
-                name_after = data[idx + 1, 7]
-                
-                if (name_before == "msc") : name_nb = 1
-                if (name_before == "eIoni") : name_nb = 2
-                if (name_before == "eBrem") : name_nb = 3
-                    
-                line_input.append(name_nb)
-                name_nb = 0
-                
-                if (name_after == "msc") : name_nb = 1
-                if (name_after == "eIoni") : name_nb = 2
-                if (name_after == "eBrem") : name_nb = 3
-                    
-                line_output.append(name_nb)
-                
-                
-                """
-                name_before = data_x[7]
-                name_after = data[idx + 1, 7]
-                if (name_before == "msc") :
-                    if (name_after == "eIoni") : 
-                        name_nb = 1
-                    else if (name_after == "eBrem") :
-                        name_nb = 2
-                        
-                else if (name_after == "msc") :
-                    if (name_before == "eIoni") : 
-                        name_nb = -1
-                    else if (name_before == "eBrem") :
-                        name_nb = -2
-                
-                else if (name_before == "eIoni") :
-                    if (name_after == "eBrem" ) :
-                        name_nb = 3
-                else if (name_after == "eIoni") :
-                    if (name_before == "eBrem" ) :
-                        name_nb = -3
-                
-                if (name_nb==0) : 
-                    raise Exception("The type of interaction was not read correctly!!")
-                """
-                
-               
-                X.append(line_input)
-                Y.append(line_output)
-
-    
-    
-    return X, Y
-
-"""DEUXIÈME VERSION !!!! """
 def creation_array(df) :
     """ This function create two dataframe X of input and the Y of output, it would be neccessary to train 
     our data and creating a model for future prediciton 
     
     Return : 
-        X : The input data composed of the following feature size (nb_inputs, 5)
-            - cos_teta
-            - Kin_E  <-- Arreter le programme quand Kin_E = 0 ! --> gérer les simulations dans ce sens
-            - dE  <-- Se calcule juste comme la différence entre les 2 énergies qu'on a !
-            - Step Lenght
-            - Proc Name
+        X : The input data composed of the following feature size (nb_inputs, 7)
+            - pos (x,y,z) ---> [0,1,2]
+            - dir (dx,dy,dz) ---> [3,4,5]
+            - KinE(MeV) ---> [6]
             
-        Y : The output data, of size (nb_inputs, 4)
-            - distance [0]
-            - delta_e [1]
+        Y : The output data, of size (nb_inputs, 11)
+            - distance ( StepLength ) [0]
+            - delta_e ( dE(MeV) )[1]
             - cos_teta [2]
-            - Q [3]
+            - Q  <--- (name_s [3], x_s [4], y_s [5], z_s [6], dx_s [7], dy_s [8], dz_s [9], KinE(MeV)_s [10])
         
     """
     #conversion of the dataframe into a numpy array :
-    data_np = df.to_numpy()
-    data = data_np[:,0:7].astype(float)
-    ProcName = data_np[:, 7]
+    df['name_s'] = df['name_s'].apply(type_to_num)
+    data = df.to_numpy()
+    #data[:,17] = data[:,17].apply(type_to_num)
     X = []
     Y = []
-    track_len = 0
     
-    for idx, data_x in enumerate(data) :
+    
+    for idx, data_x in enumerate(data[:-1]) :
     # Until we are at the end of the simulation (We don't iterate on the last element as we access in this loof the i+1 element) 
-        if (idx != data.shape[0]-1) :
-            if (data[idx+1, 0] != 0) :
-                line_input = []
-                
-                line_output = [0,0,0,0]
-                
-                #Creation of the line idx of the matrix X and Y
-                
-                #1st element:cos_teta = (dx).(dx') scalar product of the postions vectors (dx, dy, dz) at the postions i and i+1
-                # data_x[1] = Dx, data_x[2] = Dy, data_x[3] = Dz
-                cos_teta_before = data[idx-1, 1]*data_x[1] + data[idx-1, 2]*data_x[2] + data[idx-1, 3]*data_x[3]
-                cos_teta_after = data[idx+1, 1]*data_x[1] + data[idx+1, 2]*data_x[2] + data[idx+1, 3]*data_x[3]
-                line_input.append(cos_teta_before)
-                line_output[2] = (cos_teta_after)
-                
-                #2,3th elements : energy steps
-                KinE_before = data_x[4]
-                line_input.append(KinE_before)
-                
-                dE_before = data_x[5]
-                line_input.append(dE_before)
-                
-                line_output[1] = KinE_before - dE_before
-                
-                #4th element : step Length
-                step = data_x[6]
-                track_len += step
 
-                line_input.append(step)
-                line_output[0] = (track_len)
-                
-                #5th element : type of interactions (We have to use this parameter to create predict the type of interaction!
-                # no change in the type of interaction : 0
-                # change from msc to eIoni : 1 and the inverse -1
-                # change from msc to eBrem : 2 and the inverse -2
-                # change from eIoni to eBrem : 3 and the inverse -3
-                
-                name_nb = 0
-                name_before = ProcName[idx]
-                name_after = ProcName[idx + 1]
-                
-                if (name_before == "msc") : name_nb = 1
-                if (name_before == "eIoni") : name_nb = 2
-                if (name_before == "eBrem") : name_nb = 3
-                    
-                line_input.append(name_nb)
-                
-                Q = 0
-                if (name_before != name_after) :
-                    Q = 1
-                    
-                line_output[3] = Q
-                
-               
-                X.append(line_input)
-                Y.append(line_output)
-                
-            else : 
-                track_len = 0 
-                # We put back the track_len to 0 as we start from a new electron
+        if (data[idx+1, 0] != 0) :
+            line_input = []
 
-    
-    
+            #Creation of the line idx of the matrix X and Y
+            #The 3 first elements are the positions : 
+            line_input += [data_x[1], data_x[2], data_x[3]]
+            
+            # The 3,4,5th elements of X are the directions : 
+            
+            line_input += [data_x[4], data_x[5], data_x[6]]
+            
+            
+            #The 6th element of X is KinE
+            line_input.append(data_x[7])
+            
+            #Now we create the output array Y :
+            line_output = []
+            
+            #1st element is the distance corresponding to the StepLenght of data[9]
+            line_output.append(data_x[9])
+            
+            #2nd element is the change in energy corresponding to data[8]
+            line_output.append(data_x[8])
+            
+            #3rd element is the new angle :
+            #cos_teta = (dx).(dx') scalar product of the postions vectors (dx, dy, dz) at the postions i and i+1
+            # data_x[4] = Dx, data_x[5] = Dy, data_x[6] = Dz
+            
+            cos_teta = np.clip(data[idx+1, 4]*data_x[4] + data[idx+1, 5]*data_x[5] + data[idx+1, 6]*data_x[6],-1,1)
+            
+            line_output.append(cos_teta)
+
+            #The last element of the output Y will correspond to the emitted particule : 
+            if (data_x[17] == 0) : 
+                #Case where no particule is emitted, we set all the values to 0 
+                
+                line_output.append(data_x[17])
+                line_output += [0,0,0,0,0,0,0]
+                
+            elif (data_x[17] == 1 or data_x[17] == 2) : 
+                line_output.append(data_x[17])
+                line_output += [data_x[10], data_x[11], data_x[12], data_x[13], data_x[14], data_x[15], data_x[16]]
+                
+
+
+        X.append(line_input)
+        Y.append(line_output)
+
+        
     return np.asarray(X), np.asarray(Y)
 
 
